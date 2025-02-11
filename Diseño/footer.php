8d9159e367ecc7aad34d1da17d9ab2d48a938538
Peeblo-r <shrampykrampy@gmail.com>
2025-02-11 17:03:02 +0100

    jajaj

---
+++ Diseño/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <img src="footer-eu.png" width="200px">
+        <img src="footer-gob-esp.png" width="200px">
+        <img src="footer-fondos.png" width="200px">
+        <img src="logo_junta_blanco.png" width="200px">
+    </footer>
---
+++ Diseño/header.php
@@ -0,0 +1,3 @@
+<header><a href="index.html"><img src="iesjorgeguillen.svg" width="300px"></a>
+        <h1>IES LOS PEDROCHES</h1>
+    </header>
