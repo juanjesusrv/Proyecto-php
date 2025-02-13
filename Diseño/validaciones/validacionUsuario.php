7f3cf09a3e3e18c7da4e1c15f21b741c88dbf8ac
Juan Jesús Rivillas Canalejo <60579490+juanjesusrv@users.noreply.github.com>
2025-02-13 23:52:26 +0100

    Ya se introducen todos los  datos necesarios en session

--- Diseño/validaciones/validacionUsuario.php
+++ Diseño/validaciones/validacionUsuario.php
@@ -9,7 +9,7 @@
         $db = "reservas"; // Nombre de la base de datos
 
         /* Guardamos los datos del usuario en variables */
-       $contrasena = htmlspecialchars($_POST['con']); // Guardamos la contraseña en una variable
+        $contrasena = htmlspecialchars($_POST['con']); // Guardamos la contraseña en una variable
         $idUsuario = htmlspecialchars($_POST['usr']); // 
 
 
@@ -22,7 +22,28 @@
             $result = mysqli_query($con, $query); // Realizamos la consulta
 
             if (mysqli_num_rows($result) > 0) { // Si la consulta devuelve algún resultado
-                $_SESSION['idUsuario'] = $idUsuario; // Guardamos el id del usuario en la sesión
+
+                $row = mysqli_fetch_assoc($result); // Guardamos los datos del usuario en un array
+                $_SESSION['idUsuario'] = $row['idUsuario']; // Guardamos el id del usuario en una variable de sesión
+                $_SESSION['nombreUsuario'] = $row['nombreUsuario']; // Guardamos el nombre del usuario en una variable de sesión
+                $_SESSION['apellido1'] = $row['apellido1']; // Guardamos el primer apellido del usuario en una variable de sesión
+                $_SESSION['apellido2'] = $row['apellido2']; // Guardamos el segundo apellido del usuario en una variable de sesión
+                $_SESSION['email'] = $row['email']; // Guardamos el email del usuario en una variable de sesión
+                $_SESSION['idDepartamento'] = $row['idDepartamento']; // Guardamos el id del departamento del usuario en una variable de sesión
+
+                $query = "SELECT * FROM `usuarios-roles` WHERE idUsuario = '$idUsuario'"; // Creamos la consulta
+                $result = mysqli_query($con, $query); // Realizamos la consulta
+                
+                if (mysqli_num_rows($result) > 0) { // Si la consulta devuelve algún resultado
+                    $roles = array(); // Creamos un array para almacenar los roles
+                    while ($row = mysqli_fetch_assoc($result)) { // Recorremos los resultados
+                        $roles[] = $row['idRol']; // Añadimos cada rol al array
+                    }
+                    $_SESSION['roles'] = $roles; // Guardamos los roles en una variable de sesión
+                } else {
+                    echo "El usuario no tiene roles asignados"; // Si no devuelve resultados, mostramos un mensaje de error
+                }
+                
 
                 header("Location: ../###"); // Redirigimos a la página principal
             } else {
