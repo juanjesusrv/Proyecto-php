b918e66feb908246d1f1e6ce27c4dcdce608e5c7
Juan Jesús Rivillas Canalejo <60579490+juanjesusrv@users.noreply.github.com>
2025-02-13 19:40:17 +0100

    Validacion de usuario casi terminada, queda recoger el nombre de la base de datos y añadirla a una session

--- Diseño/header.php
+++ Diseño/header.php
@@ -1,4 +1,5 @@
-<header><a href="index.php"><img src="./imgs/iesjorgeguillen.svg" width="300px"></a>
+<header>
+    <a href="index.php"><img src="./imgs/iesjorgeguillen.svg" width="300px"></a>
     <h1>IES LOS PEDROCHES</h1>
 </header>
 
