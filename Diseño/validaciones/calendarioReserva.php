e5a7ecd7dcdd025f0264fec5ee1dc0663783c8b6
Rafa <rafajr90@hotmail.com>
2025-02-17 19:05:48 +0100

    inicio

--- Diseño/validaciones/calendarioReserva.php
+++ Diseño/validaciones/calendarioReserva.php
@@ -1,3 +1,16 @@
 <?php
-    
+    if ($_SERVER["REQUEST_METHOD"] == "POST"&&(isset($_POST["mesBuscar"])||isset($_POST["yearBuscar"]))) {
+        $diaSemana=date('L');
+        if ($_POST["mesBuscar"]==date('m')&&$_POST["yearBuscar"]==date('Y')){
+            $diaActual=date('d');
+            $mes=date('m');
+            $year=date('Y');
+        }
+        $mes=$_POST["mesBuscar"];
+        $year=$_POST["yearBuscar"];
+    }else{
+        $diaActual=date('d');
+        $mes=date('m');
+        $year=date('Y');
+    }
 ?>
