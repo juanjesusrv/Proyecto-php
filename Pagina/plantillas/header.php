86bcae5ddf54cca6b6ac513abe65ce44cc3fd8a6
Ruben <torricosuarezruben@gmail.com>
2025-02-18 16:31:11 +0100

    css header

--- Pagina/plantillas/header.php
+++ Pagina/plantillas/header.php
@@ -1,7 +1,7 @@
 <header>
     <a href="index.php"><img src="./imgs/iesjorgeguillen.svg" width="300px"></a>
     <h1>IES LOS PEDROCHES</h1>
-    <link rel="stylesheet" href="../Estilos/ruben.css">
+    <link rel="stylesheet" href="Estilos/ruben.css">
     <?php
     if (isset($_SESSION['idUsuario'])) {
         echo "<p>Bienvenido, ".$_SESSION['nombreUsuario']." ".$_SESSION['apellido1']." ".$_SESSION['apellido2']."</p>";
@@ -13,9 +13,9 @@
             if ($rol == 2) {
                 ?>
                 <nav class="navegacion">
-                    <ul class="menu">
-                        <li><a href="reserva.php" title="Link 1">Reservas</a></li>
-                        <li><a href="#" title="Link 2">Profesorado</a></li>
+                    <ul>
+                        <li><a href="reserva.php">Reservas</a></li>
+                        <li><a href="#">Profesorado</a></li>
                     </ul>
                 </nav>
                 <?php
