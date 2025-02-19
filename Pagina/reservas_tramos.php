870c243919634406d65f6a187868080bca4334d4
Northland <162111816+NorthlandWow@users.noreply.github.com>
2025-02-19 13:15:33 +0100

    modificacion formulario

--- Pagina/reservas_tramos.php
+++ Pagina/reservas_tramos.php
@@ -1,11 +1,12 @@
 <?php
+$array_tramos_reservaUsuario = array(); //para guardar los tramos que tiene reservados el usuario
 
 if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["fecha"])) {
     if (isset($_POST["fecha"])) {
         $array_tramos = array(); //para guardar los tramos que tienen alumnos
-        $array_tramos_reservaUsuario = array(); //para guardar los tramos que tiene reservados el usuario
+        
         $fecha = $_POST["fecha"];
-        $sql = "select * from reservas where fecha = '$fecha'"; //sacamos todas las reservas de la fecha seleccionada
+        $sql = "SELECT * FROM reservas WHERE fecha = '$fecha'"; //sacamos todas las reservas de la fecha seleccionada
         $result = mysqli_query($con, $sql);
         foreach ($result as $fila) {
             $idReserva = $fila["idReserva"];
@@ -13,14 +14,14 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["fecha"])) {
             $idAsignatura = $fila["idAsignatura"];
 
 
-            $sql3 = "select * from usuarios-asignaturas where idAsignatura = '$idAsignatura' and idUsuario = '$idUsuario'"; //sacamos el numero de alumnos de la asignatura
+            $sql3 = "SELECT * FROM `usuarios-asignaturas` WHERE idAsignatura = '$idAsignatura' AND idUsuario = '$idUsuario'";  //sacamos el numero de alumnos de la asignatura
             $numAlumnos = 0;
             $result3 = mysqli_query($con, $sql3);
             foreach ($result3 as $fila3) {
                 $numAlumnos = $fila3["numAlumnos"]; //guardamos el numero de alumnos
             }
 
-            $sql2 = "select * from reservas-tramo where idReserva = '$idReserva'"; //sacamos los tramos de la reserva
+            $sql2 = "SELECT * FROM `reservas-tramo` WHERE idReserva = '$idReserva'"; //sacamos los tramos de la reserva
             $result2 = mysqli_query($con, $sql2);
             foreach ($result2 as $fila2) {
                 $idTramo = $fila2["idTramo"];
@@ -42,8 +43,6 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["fecha"])) {
 ?>
 
 <form action="reservas_tramos.php" method="post">
-    <p>Haz tu Reserva</p>
-    <br>
     <p>Selecciona el tramo</p>
     <label for="tramo">Hora:</label>
     <select name="tramo" id="tramo">
@@ -74,5 +73,23 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["fecha"])) {
     </select>
     <br>
     <br>
-    <button type="submit">Enviar</button>
+    <label for="asignatura">Asignatura:</label>
+    <select name="asignatura" id="asignatura">
+        <?php
+        //sacamos las asignaturas del usuario
+        $sql = "SELECT ua.idAsignatura, a.nombreAsignatura 
+                FROM `usuarios-asignaturas` ua 
+                JOIN `asignaturas` a ON ua.idAsignatura = a.idAsignatura 
+                WHERE ua.idUsuario = '" . $_SESSION["idUsuario"] . "'";
+        $result = mysqli_query($con, $sql);
+        while ($asignatura = mysqli_fetch_assoc($result)) {
+            if (!in_array($asignatura["idAsignatura"], $array_tramos_reservaUsuario)) { ?>
+                <option value="<?php echo $asignatura["idAsignatura"] ?>"><?php echo $asignatura["nombreAsignatura"] ?></option>
+        <?php }
+        }
+        ?>
+    </select>
+    <br>
+    <br>
+    <button class="botones" type="submit">Enviar</button>
 </form>
