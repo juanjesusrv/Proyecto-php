94d329993cfe4773cf17e242fea7e931300b2d20
Peeblo-r <shrampykrampy@gmail.com>
2025-02-20 16:14:54 +0100

    Añadida página de error

---
+++ Pagina/errorsesion.php
@@ -0,0 +1,4 @@
+<p style="background-color:red; line-height:60px; margin:0px; font-family:arial; text-align:center; color:white; font-size: 1.5vh;">
+    No puede acceder a la aplicación sin haber iniciado sesión
+</p>
+<?php require_once "./index.php" ?>
