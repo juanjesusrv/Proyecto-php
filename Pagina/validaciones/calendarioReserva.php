523b0101d06953df08cfc165c7a080aa14154765
Rafa <rafajr90@hotmail.com>
2025-02-20 17:08:15 +0100

    Correccion calendario cuadros mal puestos

--- Pagina/validaciones/calendarioReserva.php
+++ Pagina/validaciones/calendarioReserva.php
@@ -63,9 +63,15 @@
         $calendario="<table><tr><th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th><th>Vie</th><th>Sab</th><th>Dom</th></tr>";
         if(strtotime($year."-".$mes."-01")!=1){
             $calendario.="<tr>";
-            for($i=1;$i<date("w",strtotime($year."-".$mes."-01"));$i++){
-                $calendario.="<td></td>";
-            } 
+            if(date("w",strtotime($year."-".$mes."-01"))==0){
+                for($i=1;$i<=6;$i++){
+                    $calendario.="<td></td>";
+                } 
+            }else{
+                for($i=1;$i<date("w",strtotime($year."-".$mes."-01"));$i++){
+                    $calendario.="<td></td>";
+                } 
+            }
         }
 
         for ($i=1;$i<=$ultimoDiaMes;$i++){
@@ -96,9 +102,12 @@
                 }
             if($diaSemana==7){ $calendario.="</tr>"; }
         }
-        for($i=7;$i>date("w",strtotime($year."-".$mes."-".$ultimoDiaMes));$i--){
-            $calendario.="<td></td>";
-        } 
+        if(date("w",strtotime($year."-".$mes."-".$ultimoDiaMes))!=0){
+            for($i=7;$i>date("w",strtotime($year."-".$mes."-".$ultimoDiaMes));$i--){
+                $calendario.="<td></td>";
+            }
+        }
+        echo date("w",strtotime($year."-".$mes."-".$ultimoDiaMes));
         $calendario.="</tr></table>";
         echo '<form action="reserva.php" method="post">
         <input type="hidden" name="idAsignatura" value="'.$_POST["idAsignatura"].'">
