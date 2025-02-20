4ad4d26f48cdf3634918357a4925344c3ad1000d
RafaJR90 <rafajr90@hotmail.com>
2025-02-20 03:00:20 +0100

    CalendarioSinTramosLibres

--- Pagina/validaciones/calendarioReserva.php
+++ Pagina/validaciones/calendarioReserva.php
@@ -1,63 +1,147 @@
+<p>Selecciona una asignatura</p>
+<form action="reserva.php" method="post">
+    <select name="idAsignatura" id="idAsignatura">
+        <?php
+            $sql="SELECT * from `usuarios-asignaturas` ua
+                    join asignaturas a on ua.idAsignatura=a.idAsignatura
+                    where ua.idUsuario='".$_SESSION['idUsuario']."'";
+            $resultado=mysqli_query($con, $sql);
+            while ($asignatura=mysqli_fetch_assoc($resultado)){
+                echo '<option value="'.$asignatura["idAsignatura"].'" ';
+                if (isset($_POST["idAsignatura"])&&$_POST["idAsignatura"]==$asignatura["idAsignatura"]){
+                    echo 'selected';
+                }
+                echo'>'.$asignatura["nombreAsignatura"].' - '.$asignatura["curso"].' - '.$asignatura["grupo"].'</option>';
+            }
+        ?>
+    </select>
+    <button type="submit">Seleccionar</button>
+</form>
 <?php
-    function tramosLibres(){
-        return "calenReDiaLleno";
-    }
-    $diaActual="";
-    if ($_SERVER["REQUEST_METHOD"] == "POST"&&(isset($_POST["mesBuscar"])||isset($_POST["yearBuscar"]))) {
-        if ($_POST["mesBuscar"]==date('m')&&$_POST["yearBuscar"]==date('Y')){
+    if(($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["idAsignatura"])){
+        $numeroAlumnos=numeroAlumnos($_POST["idAsignatura"]);
+        $arrayTramos=listaTramos();
+        $diaActual="";
+        if (isset($_POST["mesBuscar"])||isset($_POST["yearBuscar"])) {
+            if ($_POST["mesBuscar"]==date('m')&&$_POST["yearBuscar"]==date('Y')){
+                $diaActual=date('d');
+            }
+            $mes=$_POST["mesBuscar"];
+            $year=$_POST["yearBuscar"];
+        }else{
             $diaActual=date('d');
+            $mes=date('m');
+            $year=date('Y');
         }
-        $mes=$_POST["mesBuscar"];
-        $year=$_POST["yearBuscar"];
-    }else{
-        $diaActual=date('d');
-        $mes=date('m');
-        $year=date('Y');
-    }
-    $calendario="<table><tr>Lunes</th><th>Martes</th><th>Miercoles</th><th>Jueves</th><th>Viernes</th><th>Sabado</th><th>Domingo</th></tr>";
-    if(strtotime($year."-".$mes."-01")!=1){
-        $calendario.="<tr>";
-        for($i=1;$i<date("w",strtotime($year."-".$mes."-01"));$i++){
+        ?>
+        <P>Selecciona una fecha</P>
+        <form action="reserva.php" method="post">
+            <input type="hidden" name="idAsignatura" value="<?php echo $_POST["idAsignatura"] ?>">
+            <select name="mesBuscar" id="mesBuscar">
+            <?php 
+                $arrayMeses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+                for($i=0;$i<12;$i++){
+                    if($i<10){$numeroMes="0".($i+1);}
+                    else{$numeroMes=($i+1);}
+                    echo '<option value="'.$numeroMes.'" ';
+                    if($numeroMes==$mes){echo 'selected';}
+                    echo'>'.$arrayMeses[$i].'</option>';
+                }
+            ?>
+            </select>
+            <select name="yearBuscar" id="yearBuscar">
+            <?php
+                if(date('m')<8){echo '<option value="'.(date('Y')-1).'">'.(date('Y')-1).'</option>';}
+                echo '<option value="'.(date('Y')).'" selected>'.(date('Y')).'</option>';
+                if(date('m')>=8){echo '<option value="'.(date('Y')+1).'">'.(date('Y')+1).'</option>';}
+            ?>
+            </select>
+            <button type="submit">Seleccionar</button>
+        </form>
+        <?php
+        $ultimoDiaMes=date("t", strtotime($year."-".$mes."-01"));
+        $calendario="<table><tr><th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th><th>Vie</th><th>Sab</th><th>Dom</th></tr>";
+        if(strtotime($year."-".$mes."-01")!=1){
+            $calendario.="<tr>";
+            for($i=1;$i<date("w",strtotime($year."-".$mes."-01"));$i++){
+                $calendario.="<td></td>";
+            } 
+        }
+
+        for ($i=1;$i<=$ultimoDiaMes;$i++){
+            if($i<10){ $dia="0".$i; }
+            else{ $dia=$i; }
+            
+            $diaSemana=date("w",strtotime($year."-".$mes."-".$dia));
+            if($diaSemana==1){ $calendario.="<tr>"; }
+                if($diaSemana>0&&$diaSemana<6){
+                    if(date($year.'-'.$mes.'-'.$dia<date('Y-m-d'))){
+                        $calendario.='<td class="calenReDiaNoSeleccionable">'.$i.'</td>';
+                    }else{
+                        switch(tramosLibres($year."-".$mes."-".$dia,$_POST["idAsignatura"],$numeroAlumnos,$arrayTramos)){
+                        case "todos":
+                            $calendario.='<td class="calenReDiaVacio"><button type="submit" name="fecha" value="'.($year."-".$mes."-".$dia).'" >'.$i.'</button></td>';
+                            break;
+                        case "algunos":
+                            $calendario.='<td class="calenReDiaMedio"><button type="submit" name="fecha" value="'.($year."-".$mes."-".$dia).'" >'.$i.'</button></td>';
+                            break;
+                        case"ninguno":
+                            $calendario.='<td class="calenReDiaLleno">'.$i.'</td>';
+                            break;
+                        }
+                    }
+                    
+                }else{
+                    $calendario.='<td class="calenReDiaNoSeleccionable">'.$i.'</td>';
+                }
+            if($diaSemana==7){ $calendario.="</tr>"; }
+        }
+        for($i=7;$i>date("w",strtotime($year."-".$mes."-".$ultimoDiaMes));$i--){
             $calendario.="<td></td>";
         } 
+        $calendario.="</tr></table>";
+        echo '<form action="reserva.php" method="post">
+        <input type="hidden" name="idAsignatura" value="'.$_POST["idAsignatura"].'">
+        <input type="hidden" name="mesBuscar" value="'.$mes.'">
+        <input type="hidden" name="yearBuscar" value="'.$year.'">';
+        echo $calendario;
+        echo '</form>';
     }
 
-    for ($i=1;$i<=3;$i++){
-        if($i<10){
-            $dia="0".$i;
-        }else{
-            $dia=$i;
+
+    function numeroAlumnos($idAsignatura) {
+        $sql='SELECT * FROM `usuarios-asignaturas` WHERE idUsuario="'.$_SESSION['idUsuario'].'" and idAsignatura="'.$idAsignatura.'"';
+        $resultado=mysqli_query($con, $sql);
+        $curso=mysqli_fetch_assoc($resultado);
+        return $curso["numAlumnos"];
+    }
+
+    function listaTramos(){
+        $sql='SELECT idTramo FROM tramos ';
+        $resultado=mysqli_query($con, $sql);
+        $resul=[];
+        while($curso=mysqli_fetch_assoc($resultado)){
+            $resul[$curso["idTramo"]]=0;
         }
-        
-        $diaSemana=date("w",strtotime($year."-".$mes."-".$i));
-        if($diaSemana==1){
-            $calendario.="<tr>";
-        }
-            if($diaSemana>0&&$diaSemana<6){
-                switch(tramosLibres($year."-".$mes."-".$i)){
-                    case "todos":
-                        $calendario.='<button type="submit" name="fecha" value="'.($year."-".$mes."-".$i).'" >'.$i.'<td class="calenReDiaVacio">$i</td></button>';
-                        break;
-                    case "algunos":
-                        $calendario.='<button type="submit" name="fecha" value="'.($year."-".$mes."-".$i).'" >'.$i.'<td class="calenReDiaMedio">$i</td></button>';
-                        break;
-                    case"ninguno":
-                        $calendario.='<td class="calenReDiaLleno">$i</td>';
-                        break;
-                }
+        return $resul;
+    }
+
+    function tramosLibres($fecha,$idAsig,$numAlum,$aTramos){
+        $sql="SELECT * FROM ";
+
+        $tramosLLenos=0;
+        foreach($aTramos as $ocupado){
+            if(($ocupado+$numAlum)>100){
+                $tramosLLenos++;
             }
-        if($diaSemana==7){
-            $calendario.="</tr>";
+        }
+
+        if($tramosLLenos>=count($aTramos)){
+            return "ninguno";
+        }else if($tramosLLenos>0){
+            return "algunos";
+        }else{
+            return "todos";
         }
     }
-    
-    $calendario.="</tr>";
-    
-    for($i=1;$i<date("w",strtotime($year."-".$mes."-".strtotime('last day of this month', strtotime($year."-".$mes."-01"))));$i++){
-        $calendario.="<td></td>";
-    } 
-    $calendario.="</tr>";
-    
 ?>
-$tramo["idTramo"]." - ".$tramo["hora"]
-<button type="submit" name="fecha" value="" ></button>
